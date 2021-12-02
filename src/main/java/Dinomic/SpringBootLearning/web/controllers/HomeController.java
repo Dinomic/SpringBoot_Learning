2e422cfb8ef23db435643608e80f92cbe7faaf39
Dinomic <hthangnguyen1101@gmail.com>
2021-12-02 22:52:58 +0700

    spring security display login page

---
+++ src/main/java/Dinomic/SpringBootLearning/web/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package Dinomic.SpringBootLearning.web.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/home"})
+    public String home(){
+        return "home";
+    }
+}
