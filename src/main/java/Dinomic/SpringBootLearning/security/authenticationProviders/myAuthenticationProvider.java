7ce947e2649bd7a4c0a4ef10a5b8e7908f1abfce
tgu <tgu@elca.vn>
2021-12-30 16:13:50 +0700

    add cover + handle signIn error

--- src/main/java/Dinomic/SpringBootLearning/security/authenticationProviders/myAuthenticationProvider.java
+++ src/main/java/Dinomic/SpringBootLearning/security/authenticationProviders/myAuthenticationProvider.java
@@ -1,13 +1,14 @@
 package Dinomic.SpringBootLearning.security.authenticationProviders;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,11 +17,17 @@ public class myAuthenticationProvider implements AuthenticationProvider {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    PasswordEncoder encoder;
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getPrincipal().toString();
         String password = authentication.getCredentials().toString();
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if(!encoder.encode(password).equals(userDetails.getPassword())){
+            throw new AuthenticationCredentialsNotFoundException("bad credential");
+        }
         return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
     }
 
